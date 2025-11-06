4f1a2dcd23dfcc5e14c80dcd758c4d074a8cb452
Andrey Ivanov <Workemailaai@icloud.com>
2025-11-06 16:21:04 +0300

    add Order backend

--- server/src/routes/apiRouter.js
+++ server/src/routes/apiRouter.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const authRouter = require('./authRouter');
-// const orderRouter = require('./orderRouter');
+const orderRouter = require('./orderRouter');
 // const favouriteRouter = require('./favouriteRouter');
 // const categoryRouter = require('./categoryRouter');
 // const itemRouter = require('./itemRouter');
@@ -10,7 +10,7 @@ const apiRouter = express.Router();
 
 
 apiRouter.use('/auth', authRouter);
-// apiRouter.use('/orders', orderRouter);
+apiRouter.use('/orders', orderRouter);
 // apiRouter.use('/favourites', favouriteRouter);
 // apiRouter.use('/categories', categoryRouter);
 // apiRouter.use('/items', itemRouter);
