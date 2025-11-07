38e8490a1d733aa00033515891c8f9a4c5ac2241
DmitryMankevic <mdsinedesign@gmail.com>
2025-11-07 10:33:48 +0200

    add client fish

--- client/src/app/Layout/Layout.tsx
+++ client/src/app/Layout/Layout.tsx
@@ -1,6 +1,7 @@
 import Loader from "@/shared/HOCs/Loader/ui/Loader";
 import { useAppSelector } from "@/shared/hooks/hook";
 import Navigation from "@/widgets/Navigation/ui/Navigation";
+import Footer from "@/widgets/Footer/ui/Footer";
 import { type JSX } from "react";
 import Container from "react-bootstrap/Container";
 import { Outlet } from "react-router";
@@ -14,6 +15,8 @@ export default function Layout(): JSX.Element {
           <Navigation />
           <br />
           <Outlet />
+          <br />
+          <Footer />
         </Container>
       </Loader>
     </>
