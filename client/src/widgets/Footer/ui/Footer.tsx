85b26d2ab90aac6d28420470b6509e5f5c70cbcb
DmitryMankevic <mdsinedesign@gmail.com>
2025-11-08 15:26:28 +0200

    modify-footer

--- client/src/widgets/Footer/ui/Footer.tsx
+++ client/src/widgets/Footer/ui/Footer.tsx
@@ -8,11 +8,49 @@ export default function Footer(): JSX.Element {
       <div className={style.footerContainer}>
         <div className={style.leftComponents}>
           <NavLink to={CLIENT_ROUTES.HOME} className="nav-link text-white">
-            Главная
+            Terms & Conditions
           </NavLink>
           <NavLink to={CLIENT_ROUTES.HOME} className="nav-link text-white">
-            Новинки
+            Privacy Policy
           </NavLink>
+          <NavLink to={CLIENT_ROUTES.HOME} className="nav-link text-white">
+            Right of Withdrawal
+          </NavLink>
+          <NavLink to={CLIENT_ROUTES.HOME} className="nav-link text-white">
+            Online Ordering Process
+          </NavLink>
+          <NavLink to={CLIENT_ROUTES.HOME} className="nav-link text-white">
+            Statutory Warranty Rights
+          </NavLink>
+          <NavLink to={CLIENT_ROUTES.HOME} className="nav-link text-white">
+            Accessibility Statement
+          </NavLink>
+        </div>
+        <div className={style.centerComponents}>
+          <NavLink to={CLIENT_ROUTES.HOME} className="nav-link text-white">
+            About Us
+          </NavLink>
+          <NavLink to={CLIENT_ROUTES.HOME} className="nav-link text-white">
+            Jobs & Careers
+          </NavLink>
+          <NavLink to={CLIENT_ROUTES.HOME} className="nav-link text-white">
+            Blog
+          </NavLink>
+          <NavLink to={CLIENT_ROUTES.HOME} className="nav-link text-white">
+            Classified Ads
+          </NavLink>
+          <NavLink to={CLIENT_ROUTES.HOME} className="nav-link text-white">
+            Whistleblower system
+          </NavLink>
+        </div>
+        <div className={style.rightComponents}>
+          <NavLink to={CLIENT_ROUTES.HOME} className="nav-link text-white">
+            APP STORE
+          </NavLink>
+          <NavLink to={CLIENT_ROUTES.HOME} className="nav-link text-white">
+            GOOGLE PLAY
+          </NavLink>
+          
         </div>
       </div>
     </>
