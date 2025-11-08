645a32dd8cd85dc502979fb05d661508a5f2e768
Andrey Ivanov <Workemailaai@icloud.com>
2025-11-08 23:11:46 +0300

    add button add in cart

--- client/src/features/CardForm/ItemCard.tsx
+++ client/src/features/CardForm/ItemCard.tsx
@@ -1,8 +1,10 @@
 import type { JSX } from "react";
-import styles from "./ItemCard.module.css";
 import React, { useState } from "react";
+import styles from "./ItemCard.module.css";
 import { Heart } from "lucide-react";
-import type { IItem } from "../../entities/item/model";
+import type { IItem } from "@/entities/item/model";
+import { useAppDispatch } from "@/shared/hooks/hook";
+import { addToCartThunk } from "@/entities/cart/redux/cartThunk";
 
 type Props = {
   item: IItem;
@@ -11,6 +13,19 @@ type Props = {
 
 function ItemCard({ item }: Props): JSX.Element {
   const [liked, setLiked] = useState(false);
+  const dispatch = useAppDispatch();
+
+  const handleAddToCart = async () => {
+    try {
+      // добавляем конкретный товар по ID
+      await dispatch(addToCartThunk({ itemId: item.id, quantity: 1 }));
+
+      // временно alert, потом можно заменить на toast
+      alert(`Товар "${item.title}" добавлен в корзину`);
+    } catch (err) {
+      console.error("Ошибка при добавлении в корзину:", err);
+    }
+  };
 
   return (
     <div className={styles.card}>
@@ -29,13 +44,18 @@ function ItemCard({ item }: Props): JSX.Element {
         </button>
         <img
           src="../../public/img/yamaha-p225b.png"
-          alt="Yamaha P-225 B"
+          alt={item?.title || "Инструмент"}
           className={styles.image}
         />
       </div>
+
       <div className={styles.content}>
         <h3 className={styles.title}>{item?.title}</h3>
-        <div className={styles.price}>{item?.price} P</div>
+        <div className={styles.price}>{item?.price} ₽</div>
+
+        <button className={styles.addButton} onClick={handleAddToCart}>
+          Добавить в корзину
+        </button>
       </div>
     </div>
   );
