a8e83ec2abbafaa44cab1233759b52a89b95bd80
ericoshet <maister.bk.ru>
2025-11-11 20:29:53 +0300

    SignUpForm&Carousel&ButtonAreFixed&AddMap

--- client/src/widgets/Carousel/ui/Carousel.tsx
+++ client/src/widgets/Carousel/ui/Carousel.tsx
@@ -1,12 +1,15 @@
 import Carousel from "react-bootstrap/Carousel";
 import type { JSX } from "react";
 import { NavLink } from "react-router";
+import "@/app/App.css";
+import "./Carousel.module.css";
 
 function myCarousel(): JSX.Element {
   return (
     <Carousel>
       <Carousel.Item>
-        {/* <ExampleCarouselImage text="First slide" /> */}
+
+        {/* ПЕРВЫЙ СЛАЙД */}
         <img
           className="d-block w-100"
           src="/hotDeals.jpg"
@@ -17,25 +20,25 @@ function myCarousel(): JSX.Element {
           className="custom-carousel-caption"
           style={{ color: "#fdfdfeff", fontWeight: "bold" }}
         >
-          <h1></h1>
           <NavLink
             to="/items?marker=Hot deals"
-            className="nav-link"
+            className="navbar-brand m-0 fw-bold"
             style={{
-              color: "#fdfdfdff",
+              color: "#fce955ff",
               fontWeight: "bold",
               textShadow:
-                "2px 2px 0 #000, -2px -2px 0 #000, 2px -2px 0 #000, -2px 2px 0 #000",
+                "1px 2px 0 #000000ff, 2px 4px 0 #fc0000ff, 0 0  #04ff00ff, 0 0 0 #00b2feff",
             }}
           >
-            <h1>Горячее предложение</h1>
+            <h1 className="carousel-title-hot-deals">
+              ГОРЯЧЕЕ ПРЕДЛОЖЕНИЕ
+            </h1>
           </NavLink>
-          {/* <p>Возможное дополнение</p> */}
         </Carousel.Caption>
       </Carousel.Item>
 
       <Carousel.Item>
-        {/* <ExampleCarouselImage text="Second slide" /> */}
+        {/* ВТОРОЙ СЛАЙД /> */}
         <img
           className="d-block w-100"
           src="/news.jpg"
@@ -46,24 +49,23 @@ function myCarousel(): JSX.Element {
           className="custom-carousel-caption"
           style={{ color: "#ffffffff", fontWeight: "bold" }}
         >
-          <h1></h1>
           <NavLink
             to="/items?marker=New"
             className="nav-link"
             style={{
-              color: "#ffffffff",
+              color: "#fafafaff",
               fontWeight: "bold",
               textShadow:
-                "2px 2px 0 #000, -2px -2px 0 #000, 2px -2px 0 #000, -2px 2px 0 #000",
+                "1px 2px 0 #000000ff, 2px 4px 0 #000000ff, 0 0 0 #000000ff, 0 0 0 #000000ff",
             }}
           >
-            <h1>Новинки</h1>
+            <h1 className="carousel-title-news">новинки</h1>
           </NavLink>
         </Carousel.Caption>
       </Carousel.Item>
 
       <Carousel.Item>
-        {/* <ExampleCarouselImage text="Third slide" /> */}
+        {/* ТРЕТИЙ СЛАЙД /> */}
         <img
           className="d-block w-100"
           src="/hits.jpg"
@@ -74,28 +76,27 @@ function myCarousel(): JSX.Element {
           className="custom-carousel-caption"
           style={{ color: "#fffefbff", fontWeight: "bold" }}
         >
-          <h1></h1>
           <NavLink
             to="/items?marker=Top-Seller"
             className="nav-link"
             style={{
-              color: "#ffffffff",
+              color: "#fafafaff",
               fontWeight: "bold",
               textShadow:
-                "2px 2px 0 #000, -2px -2px 0 #000, 2px -2px 0 #000, -2px 2px 0 #000",
+                "1px 2px 0 #774d00ff, 2px 4px 0 #3a2600ff, 3px 5px 0 #000000ff, 0 0 0 #00b2feff",
             }}
           >
-            <h1> Хиты продаж </h1>
+             <h1 className="carousel-title-hots"> Хиты продаж </h1>
           </NavLink>
         </Carousel.Caption>
       </Carousel.Item>
 
       <Carousel.Item>
-        {/* <ExampleCarouselImage text="Fourth slide" /> */}
+        {/* ЧЕТВЁРЫЙ СЛАЙД /> */}
         <img
           className="d-block w-100"
           src="/Rennt.jpg"
-          alt="Third slide"
+          alt="Fourth slide"
           style={{ height: "250px", objectFit: "cover" }}
         />
         <Carousel.Caption
@@ -107,7 +108,8 @@ function myCarousel(): JSX.Element {
               "2px 2px 0 #000, -2px -2px 0 #000, 2px -2px 0 #000, -2px 2px 0 #000",
           }}
         >
-          <h4>скоро открытие</h4>
+          <p className="carousel-title-xl"
+          >скоро открытие</p>
           <a
             href="#instrument-rental-card"
             className="nav-link"
@@ -118,7 +120,7 @@ function myCarousel(): JSX.Element {
                 "2px 2px 0 #000, -2px -2px 0 #000, 2px -2px 0 #000, -2px 2px 0 #000",
             }}
           >
-            <h1>Прокат инструментов</h1>
+             <h1 className="carousel-title-rennt">Прокат инструментов</h1>
           </a>
         </Carousel.Caption>
       </Carousel.Item>
